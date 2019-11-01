debbb25824681f5f14f9db5ec4c34cccfe2a4be2
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-11-02 00:32:38 +0200

    add index

---
+++ src/scripts/develop/develop_1.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.reviews').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
+
+$(window).load(function(){
+
+});
+
+$(window).resize(function(){
+
+});
