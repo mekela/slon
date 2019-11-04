a70f5a65adb25a0619741a3ea6210d79a7a8a583
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-11-04 10:02:09 +0200

    build

--- src/scripts/develop/develop_1.js
+++ src/scripts/develop/develop_1.js
@@ -1,13 +1,54 @@
 $(document).ready(function(){
+
+    //slider
     $('.reviews').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            }
+        ]
     });
     $('.slider').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        dots: true,
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            }
+        ]
+    });
+
+    //toggle
+    $( ".footer__center h6" ).click(function() {
+        $( this ).toggleClass( "active" );
+    });
+
+    //menu
+    $( ".header__trigger" ).click(function() {
+        $( '.mobile-menu' ).addClass( "active" );
+        $( '.mobile-menu__overlay' ).show(  );
+    });
+    $( ".mobile-menu__overlay, .mobile-menu__close" ).click(function() {
+        $( '.mobile-menu' ).removeClass( "active" );
+        $( '.mobile-menu__overlay' ).hide(  );
     });
 });
 
